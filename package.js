7c3f26f6f0aec1cde63255260fce8dc9c0ab7fa9
Redhaxx000 <redhaxx1@outlook.com>
2025-06-06 12:40:31 +0530

    Create package.js

---
+++ package.js
@@ -0,0 +1,13 @@
+{
+  "name": "vanir-afk-bot",
+  "version": "1.0.0",
+  "description": "AFK bot for Aternos with Render deploy",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "mineflayer": "*",
+    "http": "^0.0.1-security"
+  }
+}
